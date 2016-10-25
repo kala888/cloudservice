1ffc60d8f4fc29746a196b3a0ab5b78c9caf70e2
kala888 <kala.niubility@gmail.com>
2016-10-25 15:56:02 +0800

    remove unused project

--- reg-server/src/main/java/acp/accs/Application.java
+++ reg-server/src/main/java/acp/accs/Application.java
@@ -2,10 +2,12 @@ package acp.accs;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaServer
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).web(true).run(args);
