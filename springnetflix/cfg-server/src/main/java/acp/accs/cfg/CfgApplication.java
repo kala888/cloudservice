e4d7dbb42f78d6f9c437f71f9f8c5a46e25848f9
kala888 <kala.niubility@gmail.com>
2016-11-17 13:42:42 +0800

    add changes

---
+++ springnetflix/cfg-server/src/main/java/acp/accs/cfg/CfgApplication.java
@@ -0,0 +1,15 @@
+package acp.accs.cfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableConfigServer
+public class CfgApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CfgApplication.class, args);
+    }
+}
